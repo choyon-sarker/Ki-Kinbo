cdcfb4493311fb5de1679378ba45a9099cac51ed
choyon-sarker <sarkerchoyon123@gmail.com>
2024-10-28 16:41:37 +0600

    firebase dded for entire app to use

--- app/src/main/java/com/devdroid/kikinbo/MainActivity.kt
+++ app/src/main/java/com/devdroid/kikinbo/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    val database = FirebaseDatabase.getInstance()
+    val databaseRef = database.getReference("Products")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        pushProductData()
+    }
+    private fun pushProductData() {
+        databaseRef.push().setValue(product1)
+        databaseRef.push().setValue(product2)
+        databaseRef.push().setValue(product3)
+        databaseRef.push().setValue(product4)
+        databaseRef.push().setValue(product5)
+        databaseRef.push().setValue(product6)
+            .addOnSuccessListener {
+                println("Product data added successfully!")
+            }
+            .addOnFailureListener { e ->
+                println("Failed to add product data: ${e.message}")
+            }
     }
 }
