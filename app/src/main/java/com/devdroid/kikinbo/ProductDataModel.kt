6181033cc70756b062ee9a700c16907669f027dd
Yumna-Yumi <jucse29.369@gmail.com>
2024-10-30 13:52:57 +0600

    rate and review items functionality added

---
+++ app/src/main/java/com/devdroid/kikinbo/ProductDataModel.kt
@@ -0,0 +1,15 @@
+package com.devdroid.kikinbo
+
+data class ProductDataModel(
+    val productId: String? = null,
+    val productName: String? = null,
+    val productPrice: Int? = null,
+    val productDetail: String? = null,
+    val productCategory: String? = null,
+    val reviews: MutableList<Review>? = arrayListOf()  // To store multiple reviews
+)
+
+data class Review(
+    var productRating: String? = null,
+    var productIdReview: String? = null
+)
