61fa4bc28ba9c21287fa6473b55e04157a1d914d
RaianRashidRimon <rimonraian2@gmail.com>
2024-10-31 21:50:51 +0600

    dokka plugins added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.dokka.gradle.plugin)
 }
