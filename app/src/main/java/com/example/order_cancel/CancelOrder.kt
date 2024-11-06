27b1dc37e31913dc3ee318a055ebb206c408161b
lamia101 <lamialatif01@gmail.com>
2024-11-06 17:24:37 +0600

    cancel order layout added

---
+++ app/src/main/java/com/example/order_cancel/CancelOrder.kt
@@ -0,0 +1,20 @@
+package com.example.order_cancel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CancelOrder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cancel_order)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
